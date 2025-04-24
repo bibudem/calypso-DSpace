39ffa194f899d53d82675aa8a1f431a25cc99622
Natalia Jabinschi <natalia.jabinschi@umontreal.ca>
2025-04-24 08:54:23 -0400

    Modifications mineures pour ajouter des commentaires sur les modifications locale.

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/security/WebSecurityExpressionEvaluator.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/security/WebSecurityExpressionEvaluator.java
@@ -12,7 +12,9 @@ import java.util.List;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+/* BibUdeM add: import pour les erreurs de Spring*/
 import org.springframework.context.annotation.Lazy;
+/* fin*/
 import org.springframework.core.GenericTypeResolver;
 import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.Expression;
@@ -47,6 +49,8 @@ public class WebSecurityExpressionEvaluator {
      * Constructor for this class that sets all the {@link SecurityExpressionHandler} objects in a list
      * @param securityExpressionHandlers    The {@link SecurityExpressionHandler} for this class
      */
+     /* BibUdeM add: modifications apportées pour dépanner les erreurs au démarrage de Spring.
+    public WebSecurityExpressionEvaluator(List<SecurityExpressionHandler> securityExpressionHandlers) {*/
     public WebSecurityExpressionEvaluator(@Lazy List<SecurityExpressionHandler> securityExpressionHandlers) {
         this.securityExpressionHandlers = securityExpressionHandlers;
     }
